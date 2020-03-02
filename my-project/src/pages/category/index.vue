f0be38730ea2f9cae856ca41dc811b151e8e0afc
zhen <553522085@qq.com>
2020-03-02 12:20:08 +0800

    weixin

---
+++ my-project/src/pages/category/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="category">
+		<div class="search">
+			<div class="ser">
+				<span class="icon"></span>
+				<span>商品搜索</span>
+			</div>
+		</div>
+		<div class="content">
+			<scroll-view class="left" scrool-y="true">
+				<div class="iconText">居家</div>
+				<div class="iconText">居家</div>
+				<div class="iconText">居家</div>
+			</scroll-view>
+			<scroll-view class="right" scrool-y="true">
+				<div class="banner">
+					<img src="" alt="">
+				</div>
+				<div class="title">
+					<span>-</span>
+					<span>志趣分类</span>
+					<span>-</span>
+				</div>
+				<div class="bottom">
+					<div class="item">
+						<img src="" alt="">
+						<span>name</span>
+					</div>
+				</div>
+			</scroll-view>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style  scoped>
+
+</style>
